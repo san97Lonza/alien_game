8984b5bba2af254dc911f2354b58c4d6367412a0
SK <115445955+san97Lonza@users.noreply.github.com>
2024-04-19 23:04:32 +0800

    Adding changes like the ship speed, refractoring the event checker and limiting the ship range

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,22 +14,63 @@ class AlienInvasion:
 
         #to a diaplay window and assign it to a attribute so that the display will be available to all the methods
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invadion")
         self.ship = Ship(self)
     
     def run_game(self):
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            #for loop will check for a list of events that have happen and perform tasks depending on the kinds of events that occur
+    #these helper method are created to help simplify the tasks
+    #for example: _check_events() will help manage events
+    #while _update_screen() will handle the updating of screen with the latest updates
+
+    def _check_events(self):
+        #for loop will check for a list of events that have happen and perform tasks depending on the kinds of events that occur
             for event in pygame.event.get():
 
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #this pygame.display.flip() will update the latest changes to the screen
-            pygame.display.flip()
+
+                #these confitional statements check for right or left key when a key is pressed and update the ship movement falg accordingly
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                
+                #these confitional statements check for right or left key when a key is realsed and update the ship movement falg accordingly
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+                    
+    #Refractoring the event checker for right and left keys when they are pressed or realeased
+    def _check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        #quits the game when q is pressed
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    
+    def _check_keyup_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #this pygame.display.flip() will update the latest changes to the screen
+        pygame.display.flip()
+         
+        
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -4,11 +4,32 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #store decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        #Using the movement flag to update the ships's movement
+        #using multipe if statemets to check for when both keys are pressed down together
+        #updating the ship's x value
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -=1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
